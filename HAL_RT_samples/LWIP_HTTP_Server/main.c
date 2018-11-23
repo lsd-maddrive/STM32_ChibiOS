5266e33a09aff7b2dfd25ba632b93b1bb760718e
KaiL4eK <darklexi494@gmail.com>
2018-11-23 11:56:21 +0300

    [HAL_RT_samples]: AddedHTTP server sample with LWIP (Ethernet + TCP/IP Stack)

---
+++ HAL_RT_samples/LWIP_HTTP_Server/main.c
@@ -0,0 +1,80 @@
+/*
+    ChibiOS - Copyright (C) 2006..2016 Giovanni Di Sirio
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+#include "ch.h"
+#include "hal.h"
+
+#include "chprintf.h"
+
+#include "lwipthread.h"
+#include "web/web.h"
+
+/*===========================================================================*/
+/* Main and generic code.                                                    */
+/*===========================================================================*/
+
+/*
+ * Green LED blinker thread, times are in milliseconds.
+ */
+static THD_WORKING_AREA(waThread1, 128);
+static THD_FUNCTION(Thread1, arg)
+{
+  (void)arg;
+  chRegSetThreadName( "blinker" );
+  while ( true )
+  {
+    palToggleLine( LINE_LED1 );
+    chThdSleepMilliseconds( 1000 );
+  }
+}
+
+/*
+ * Application entry point.
+ */
+int main(void)
+{
+  /*
+   * System initializations.
+   * - HAL initialization, this also initializes the configured device drivers
+   *   and performs the board-specific initializations.
+   * - Kernel initialization, the main() function becomes a thread and the
+   *   RTOS is active.
+   * - lwIP subsystem initialization using the default configuration.
+   */
+  halInit();
+  chSysInit();
+  lwipInit(NULL);
+
+  /*
+   * Creates the blinker thread.
+   */
+  chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
+
+  /*
+   * Creates the HTTP thread (it changes priority internally).
+   */
+  chThdCreateStatic(wa_http_server, sizeof(wa_http_server), NORMALPRIO + 1,
+                    http_server, NULL);
+
+
+  while (true)
+  {
+    chThdSleepSeconds( 1 );
+  }
+}

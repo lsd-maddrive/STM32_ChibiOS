4ede1311b838ac87a055f85556f08d8181dbd38d
Doraepod <47301229+Doraepod@users.noreply.github.com>
2022-12-07 16:51:31 +0300

    Add comments to load.bat

--- ProjectTemplates/RT-STM32F103C8-Bluepill/load.bat
+++ ProjectTemplates/RT-STM32F103C8-Bluepill/load.bat
@@ -1,5 +1,8 @@
+:: Full memory erase
 st-link_cli -ME
+:: Load ch.bin file from /build folder to memory 
 st-link_cli -P %~dp0/build/ch.bin 0x08000000
+:: Reset program to start execution
 st-link_cli -Rst
 
 :pause

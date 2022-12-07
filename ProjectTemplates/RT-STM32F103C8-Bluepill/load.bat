11b42cde9cac644f5b518d25aef8edcc6e2eac57
Doraepod <47301229+Doraepod@users.noreply.github.com>
2022-12-07 18:25:31 +0300

    Add binary path file to variable

--- ProjectTemplates/RT-STM32F103C8-Bluepill/load.bat
+++ ProjectTemplates/RT-STM32F103C8-Bluepill/load.bat
@@ -1,7 +1,9 @@
+set BIN="%~dp0/build/ch.bin"
+
 :: Full memory erase
 st-link_cli -ME
 :: Load ch.bin file from /build folder to memory 
-st-link_cli -P %~dp0/build/ch.bin 0x08000000
+st-link_cli -P %BIN% 0x08000000
 :: Reset program to start execution
 st-link_cli -Rst
 
